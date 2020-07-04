51629dd843da7d070c21efae6dcb3631f12d9fe7
Jefferson Luís <jeffluipe@gmail.com>
2020-07-04 18:11:50 -0300

    Nível 2 - Desafio 1

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
 import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,31 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const {income, outcome} = this.transactions.reduce((accumulator, transaction) => {
+      if(transaction.type == 'income') accumulator.income += transaction.value;
+      if(transaction.type == 'outcome') accumulator.outcome += transaction.value;
+      return accumulator;
+    },{
+      income:0,
+      outcome:0
+    });
+
+    const total = income - outcome;
+
+    return {income, outcome, total};
+
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title, value, type}: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({title, value, type});
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
